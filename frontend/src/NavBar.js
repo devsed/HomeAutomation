30e6bd7362659d01e01cc28db522209d9c01d082
Jukka Lindberg <jukka.lindberg.jl@gmail.com>
2019-01-30 13:51:42 +0200

    NavBar restored to original.

--- frontend/src/NavBar.js
+++ frontend/src/NavBar.js
@@ -6,13 +6,16 @@ import { connect } from 'react-redux';
 
 class NavBar extends React.Component {
 
+
+
     logout = () => {
         this.props.dispatch(logout());
     }
 
     render() {
         let navbar;
-        if (this.props.isLogged) {
+
+		if (this.props.isLogged) {
             navbar = <List horizontal>
                 <List.Item>
                     <Link name="logout"
@@ -24,18 +27,13 @@ class NavBar extends React.Component {
             </List>
         } else {
             navbar = <div style={{ height: 65 }} />
-        }
-        if (this.props.isLogged && this.props.actionError.length > 0) {
+		}
+
+		if (this.props.loginError.length > 0) {
             navbar = <div style={{ height: 65 }}>
-                <p>Error: {this.props.actionError}</p>
-				<List horizontal>
-					<List.Item>
-						<Link name="logout"
-							to="/" onClick={this.logout}>Logout</Link>
-					</List.Item>
-				</List>
-			</div>
-        }
+                <p>Error:{this.props.loginError}</p></div>
+		}
+		
         return (
             <div style={{ height: 65 }}>
                 {navbar}
@@ -45,24 +43,16 @@ class NavBar extends React.Component {
 }
 
 const mapStateToProps = (state) => {
-	let loading=false;
-	let actionErrors = [
-		state.login.error,
-		state.home.error,
-		state.room.error,
-		state.device.error,
-		state.function.error
-	]
-
-	function getError(actionError) {
-		return actionError.length > 0;
-	}
-	let error = actionErrors.find(getError);
+    let loading=false;
+    let error="";
+    if(state.login.error.length>0) {
+        error=state.login.error
+    }
 
     return {
         isLogged: state.login.isLogged,
         loading: loading,
-        actionError: error !== undefined ? error : ""
+        loginError: error
     }
 }
 
